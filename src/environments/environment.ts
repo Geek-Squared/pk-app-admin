efd4807932d4618704d41406d7ed53d261e22bba
onealmrnge <oneal@quatrohaus.com>
2021-01-28 19:22:22 +0200

    messages display

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCM-e-V6Y4rw_KNO95Zs2bZmCeTnI1h95I',
-    authDomain: 'positive-konnections-578ca.firebaseapp.com',
-    databaseURL: 'https://positive-konnections-578ca.firebaseio.com',
-    projectId: 'positive-konnections-578ca',
-    storageBucket: 'positive-konnections-578ca.appspot.com',
-    messagingSenderId: '48630767331',
-    appId: '1:48630767331:web:677633a21316dc5a182fd6',
-    measurementId: 'G-WWL2TLJFMJ',
+    apiKey: 'AIzaSyAyQhv2xlj15JF7_o7DbBLRSB4XTXQL1FQ',
+    authDomain: 'positive-konnections-42d8a.firebaseapp.com',
+    databaseURL: 'https://positive-konnections-42d8a.firebaseio.com',
+    projectId: 'positive-konnections-42d8a',
+    storageBucket: 'positive-konnections-42d8a.appspot.com',
+    messagingSenderId: '803337097020',
+    appId: '1:803337097020:web:ba78819354d9754930d839',
+    measurementId: 'G-RLJPEC09H2',
   },
 };
